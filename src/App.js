87de6fe5fa1d9e523930a60918dd1843f0a57c4f
Hatim Shabbir <hatim.shabbir5152@gmail.com>
2021-06-12 02:07:55 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './Components/Navbar.js';
+
+
+const App = () => {
+  return (
+    <div className="app">
+     <Navbar/>
+    </div>
+  );
+}
+
+export default App;
