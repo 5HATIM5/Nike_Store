4690105f6eadc42c5761f161364e4d48d3fdfeaa
Hatim Shabbir <hatim.shabbir5152@gmail.com>
2021-07-14 22:00:22 +0500

    Burger navbar done

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -1,29 +1,37 @@
 import React from "react";
 import "./MainPage.css";
-import nike1 from "../Assets/nike1.png";
+import nike from "../Assets/nike.png";
 import ExpandMoreOutlinedIcon from "@material-ui/icons/ExpandMoreOutlined";
+import Zoom from "react-reveal/Zoom";
+import Fade from "react-reveal/Fade";
 
 const MainPage = () => {
   return (
     <div className="mainPage">
       <div className="radial">
         <div className="mainPage_radial">
-          <img src={nike1} alt="" />
+          <Zoom>
+            <div>
+              <img src={nike} className="Nike_img" alt="Nike shoe" />
+            </div>
+          </Zoom>
         </div>
       </div>
       <div className="mainPage_content">
-        <p className="content_Tagline">BASKETBALL SHOE</p>
-        <p className="content_Heading">LeBron16</p>
-        <p className="content_Matter">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-          voluptatibus facere perferendis aliquam hic assumenda qui tenetur
-          similique unde sit ut, iusto commodi nemo fuga repellendus aliquid
-          fugit veritatis vitae.
-        </p>
-        <button className="button_Shopnow">SHOP NOW</button>
+        <Fade bottom>
+          <p className="content_Tagline">BASKETBALL SHOE</p>
+          <p className="content_Heading">LeBron16</p>
+          <p className="content_Matter">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            voluptatibus facere perferendis aliquam hic assumenda qui tenetur
+            similique unde sit ut, iusto commodi nemo fuga repellendus aliquid
+            fugit veritatis vitae.
+          </p>
+          <button className="button_Shopnow">SHOP NOW</button>
+        </Fade>
       </div>
 
-      <ExpandMoreOutlinedIcon className="arrow" fontSize="large"/>
+      <ExpandMoreOutlinedIcon className="arrow" fontSize="large" />
     </div>
   );
 };
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,30 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../Assets/logo.png";
+import { IconButton } from "@material-ui/core";
 import ShoppingCartOutlinedIcon from "@material-ui/icons/ShoppingCartOutlined";
 import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
 import AppsOutlinedIcon from "@material-ui/icons/AppsOutlined";
-import { IconButton } from "@material-ui/core";
 import ExpandMoreOutlinedIcon from "@material-ui/icons/ExpandMoreOutlined";
+import CloseOutlinedIcon from "@material-ui/icons/CloseOutlined";
+
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [burgerStatus, setBurgerStatus] = useState(false);
+
   return (
-    <div className="navbar">
+    <div className="navbar flex-center-sb">
       <img className="navbar_logo" src={logo} alt="Nike_Logo" />
 
-      <div className="navbar_links">
+      <div className="navbar_links flex-center-sb text-Style">
         <p>
           MEN
-          <ExpandMoreOutlinedIcon style={{color:"#808080"}}/>
+          <ExpandMoreOutlinedIcon style={{ color: "#808080" }} />
         </p>
 
         <p>
           WOMEN
-          <ExpandMoreOutlinedIcon style={{color:"#808080"}}/>
+          <ExpandMoreOutlinedIcon style={{ color: "#808080" }} />
         </p>
         <p>
           KIDS
-          <ExpandMoreOutlinedIcon style={{color:"#808080"}}/>
+          <ExpandMoreOutlinedIcon style={{ color: "#808080" }} />
         </p>
         <p>CUSTOMIZE</p>
       </div>
@@ -37,8 +41,36 @@ const Navbar = () => {
           <SearchOutlinedIcon fontSize="small" />
         </IconButton>
         <IconButton className="navbar_icon">
-          <AppsOutlinedIcon fontSize="small" />
+          <AppsOutlinedIcon
+            onClick={() => setBurgerStatus(true)}
+            fontSize="small"
+            className="Apps_iconbutton"
+          />
         </IconButton>
+        <div className={burgerStatus ? "burger_Nav_active " : "burger_Nav"}>
+          <div className="burger_Nav_closeIcon">
+            <IconButton>
+              <CloseOutlinedIcon
+                style={{ color: "#000000" }}
+                onClick={() => setBurgerStatus(false)}
+              />
+            </IconButton>
+          </div>
+          <p>
+            MEN
+            <ExpandMoreOutlinedIcon style={{ color: "#808080" }} />
+          </p>
+
+          <p>
+            WOMEN
+            <ExpandMoreOutlinedIcon style={{ color: "#808080" }} />
+          </p>
+          <p>
+            KIDS
+            <ExpandMoreOutlinedIcon style={{ color: "#808080" }} />
+          </p>
+          <p>CUSTOMIZE</p>
+        </div>
       </div>
     </div>
   );
